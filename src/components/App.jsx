fbc4358544f1387d0ea67d93dc24a519609053bf
Bazykin11 <103375706+Bazykin11@users.noreply.github.com>
2023-01-24 21:47:09 +0200

    up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
+import { Movies } from '../pages/Movies';
+import { Home } from '../pages/Home';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Routes>
+        <Route>path="/" element={<Home />}</Route>
+        <Route>path="/movies" element={<Movies />}</Route>
+      </Routes>
+    </>
   );
 };
